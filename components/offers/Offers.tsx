2d9c940829da45cd61cb66bc6c2783b8c19269b4
Kaspar Naaber <kaspar.naaber@gmail.com>
2022-05-16 15:58:13 +0300

    Add basic content for the offers section

--- components/offers/Offers.tsx
+++ components/offers/Offers.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
 
+import styles from './Offers.module.scss';
+
+const offers = [
+  {
+    title: 'Õppimine ja areng',
+    features: [
+      'Ühised hackatonid',
+      'Mentorid',
+      'Iseõppimise päevad',
+      'Vertikaaliüritused ja siseseminarid',
+      'Iglu raamatukogu',
+      'Töö ja kool käsikäes',
+    ],
+  },
+  {
+    title: 'Õppimine ja areng',
+    features: [
+      'Ühised hackatonid',
+      'Mentorid',
+      'Iseõppimise päevad',
+      'Vertikaaliüritused ja siseseminarid',
+      'Iglu raamatukogu',
+      'Töö ja kool käsikäes',
+    ],
+  },
+  {
+    title: 'Õppimine ja areng',
+    features: [
+      'Ühised hackatonid',
+      'Mentorid',
+      'Iseõppimise päevad',
+      'Vertikaaliüritused ja siseseminarid',
+      'Iglu raamatukogu',
+      'Töö ja kool käsikäes',
+    ],
+  },
+];
+
 export const Offers: React.FC = () => {
-  return <div>bla</div>;
+  return (
+    <div className={styles.offers}>
+      <h2>Me pakume</h2>
+
+      <div className={styles.offers__content}>
+        {offers.map((offer, index) => (
+          <div key={index}>
+            <h3>{offer.title}</h3>
+            <ul>
+              {offer.features.map((feature, index) => (
+                <li key={index}>feature</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Offers;
