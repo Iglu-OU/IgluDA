9e0402f8ea244253b4cc86a3dc18ca944c8ac90f
Ivar Kallejärv <ivar.kallejarv@iglu.ee>
2022-05-16 19:41:11 +0300

    Remove PWA from build temporarily

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
-const withPWA = require('next-pwa');
+// TODO Stopped working for some reason
+// const withPWA = require('next-pwa');
 
-module.exports = withPWA({
-  webpack5: true,
-  target: 'experimental-serverless-trace',
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-  },
+module.exports = {
   webpack(config) {
     config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] });
     return config;
   },
-  experimental: {
-    concurrentFeatures: true,
-    // Could be useful, but some hooks are not supported so requires a small rewrite
-    // serverComponents: true,
-  },
-});
+};
