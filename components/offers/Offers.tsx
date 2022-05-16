9db7e5c7b3282ce42d9065755672cf7ab1d207a9
Anna-Liisa Saks <anna-liisa.saks@iglu.ee>
2022-05-16 18:22:13 +0300

    Add offers styles

--- components/offers/Offers.tsx
+++ components/offers/Offers.tsx
@@ -6,50 +6,45 @@ const offers = [
   {
     title: 'Õppimine ja areng',
     features: [
-      'Ühised hackatonid',
       'Mentorid',
       'Iseõppimise päevad',
-      'Vertikaaliüritused ja siseseminarid',
+      'Ühised häckatonid',
+      'Konverentsid ja siseseminarid',
       'Iglu raamatukogu',
       'Töö ja kool käsikäes',
     ],
   },
   {
-    title: 'Õppimine ja areng',
+    title: 'Puhkus ja vabadus',
     features: [
-      'Ühised hackatonid',
-      'Mentorid',
-      'Iseõppimise päevad',
-      'Vertikaaliüritused ja siseseminarid',
-      'Iglu raamatukogu',
-      'Töö ja kool käsikäes',
+      'Paindlik tööaeg',
+      'Jõuluvaheaeg ehk lisapuhkepäevad',
+      'Haiguspäevade kompoensatsioon',
+      'Puhkepäevad tähtpäevade puhul',
     ],
   },
   {
-    title: 'Õppimine ja areng',
-    features: [
-      'Ühised hackatonid',
-      'Mentorid',
-      'Iseõppimise päevad',
-      'Vertikaaliüritused ja siseseminarid',
-      'Iglu raamatukogu',
-      'Töö ja kool käsikäes',
-    ],
+    title: 'Tervis ja heaolu',
+    features: ['Massaaž kontorites', 'Spordikompoenstatsioon', 'Iglu tervisekuu'],
+  },
+  {
+    title: 'Üritused ja ühine aeg',
+    features: ['Suve- ja talvepäevad', '"Tunne Eskimot" üritused', 'Pannkoogid ja smuutihommikud'],
   },
 ];
 
 export const Offers: React.FC = () => {
   return (
     <div className={styles.offers}>
-      <h2>Me pakume</h2>
-
+      <h2>Miks meiega on hea koos ehitada?</h2>
       <div className={styles.offers__content}>
         {offers.map((offer, index) => (
-          <div key={index}>
-            <h3>{offer.title}</h3>
-            <ul>
+          <div key={index} className={styles.offers__item}>
+            <h3 className={styles.offers__title}>{offer.title}</h3>
+            <span className={styles.offers__separator} />
+            <ul className="list-unstyled">
               {offer.features.map((feature, index) => (
-                <li key={index}>feature</li>
+                <li key={index}>{feature}</li>
               ))}
             </ul>
           </div>
