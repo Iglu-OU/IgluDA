42d7d43118053095d69793c691dbc209b3879776
Rasmus Kilk <rasmus.kilk@iglu.ee>
2026-04-15 11:01:57 +0300

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-// TODO Stopped working for some reason
-// const withPWA = require('next-pwa');
-
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] });
-    return config;
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   },
 };
